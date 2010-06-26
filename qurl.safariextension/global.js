2c81e9402eccb1c0e972df5606f5ae6128c85e1d
Simon Evans <si@si.org>
2010-06-26 17:57:08 +0000

    Add 'Queue Current Page' option
    Merge handlers for validate messages
    
    Merge handles for command messages
    
    Tidy up javascript

--- qurl.safariextension/global.js
+++ qurl.safariextension/global.js
@@ -1,34 +1,28 @@
-// Contextual Menu item
+// Contextual Menu and Toolbar item
 safari.application.addEventListener("command", qURLCommand, false);
 safari.application.addEventListener("validate", qURLValidate, false);
 
-// Toolbar Item
-safari.application.addEventListener("command", qNextURLCommand, false);
-safari.application.addEventListener("validate", qNextURLValidate, false);
-
-// Message from index page
+// Messages from code in URL page display
 safari.application.addEventListener("message", messageHandler, false);
-
 
 
 {
     var urlPage = null;
-    var urlQueue = safari.extension.settings.urlQueue;
-    if(urlQueue === undefined)
-        urlQueue = new Array();
+    var urlQueue = safari.extension.settings.urlQueue || [];
 
     function updatedQueue() {
         safari.extension.settings.urlQueue = urlQueue;
-        for(var i in safari.extension.toolbarItems) {
+        for(var i = 0; i < safari.extension.toolbarItems.length; i++) {
             safari.extension.toolbarItems[i].disabled = !urlQueue.length;
-            safari.extension.toolbarItems[i].badge = safari.extension.settings.showbadge ? urlQueue.length : 0;
+            safari.extension.toolbarItems[i].badge = safari.extension.settings.showbadge && urlQueue.length;
         }
     }
 
 
     function messageHandler(event) {
-        if(event.name === "sendQueue")
+        if(event.name === "sendQueue") {
             event.target.page.dispatchMessage("URLQueue", urlQueue);
+        }
 
         if(event.name == "removeURL") {
             var idx = event.message;
@@ -42,57 +36,41 @@ safari.application.addEventListener("message", messageHandler, false);
 
 
     function qURLValidate(event) {
-        if(event.command !== "qurl")
-            return;
-
-        var ui = null;
-        try {
-            ui = event.userInfo;
-        } catch(e) {
-            ui = null;
-        }
-
-        if(ui === undefined || ui == null || ui.href === undefined) {
+        if(event.command === "qurl" || event.command === "qpage") {
             event.target.disabled = true;
-        } else {
-            event.target.disabled = false;
+            try {
+                var ui = event.userInfo;  // This can throw an error
+
+                if(event.command === "qurl" && ui.href !== undefined)
+                    event.target.disabled = false;
+
+                else if(event.command === "qpage" && ui.curpage !== undefined && ui.href === undefined)
+                    event.target.disabled = false;
+
+            } catch(e) {
+            }
+        } else if(event.command === 'qurlbutton') {
+            event.target.disabled = !urlQueue.length;
+            event.target.badge = safari.extension.settings.showbadge && urlQueue.length;
         }
     }
 
 
     function qURLCommand(event) {
-        if(event.command !== "qurl")
-            return;
-        
-        try {
-            var ui = event.userInfo;
-            urlQueue.push(ui);
-            updatedQueue();
-            if(urlPage != null) 
-                urlPage.page.dispatchMessage("URLQueue", urlQueue);
-        } catch(e) {
-        }
-    }
-
-
-    function qNextURLValidate(event) {
-        if(event.command !== 'qurlbutton')
-            return;
-        
-        if(urlQueue.length == 0)
-            event.target.disabled = true;
-        else
-            event.target.disabled = false;
-
-        event.target.badge = safari.extension.settings.showbadge ? urlQueue.length : 0;
-    }
-
-
-    function qNextURLCommand(event) {
-        if(event.command !== 'qurlbutton')
-            return;
-
-        if(urlQueue.length > 0) {
+        if(event.command === "qurl" || event.command === "qpage") {
+            try {
+                var ui = event.userInfo;
+                if(event.command === "qpage") {
+                    ui.href = ui.curpage;
+                    ui.name = ui.title;
+                }
+                urlQueue.push(ui);
+                updatedQueue();
+                if(urlPage != null)
+                    urlPage.page.dispatchMessage("URLQueue", urlQueue);
+            } catch(e) {
+            }
+        } else if(event.command === 'qurlbutton' && urlQueue.length > 0) {
             if(safari.extension.settings.buttonmode == 1) {
                 var next = urlQueue.shift();
                 safari.application.activeBrowserWindow.activeTab.url = next.href;
